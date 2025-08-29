7500ce80b191f5c1eda0410b858136e447f06cf5
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-08-29 19:38:38 +0200

    feat(page): add news page

---
+++ src/app/news/page.tsx
@@ -0,0 +1,12 @@
+
+export default function News() {
+  return (
+    <div>
+      <div className="bg-dark w-full h-[235px] flex items-center justify-center pt-[60px] pb-[43px]">
+        <h2 className="text-beige text-[60px] text-center whitespace-pre-line leading-[1.1]">
+          {`CAPAnalysis\nin the news`}
+        </h2>
+      </div>
+    </div>
+  );
+}
