1932f3db976b8efbf47509a02c9d1f4453c2bb3a
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-08-29 18:55:53 +0200

    feat(page): add ForensicFinancialAnalysis with Hero component

---
+++ src/app/forensic-financial-analysis/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/common/Hero";
+
+export default function ForensicFinancialAnalysis() {
+  return (
+    <div>
+      <Hero
+        imgSrc="/assets/images/hero_ffa.jpeg"
+        imgAlt=""
+        title={`Forensic financial\n analysis`}
+        text={`When things go wrong, we bring clarity,\n evidence, and impact.`}
+      />
+    </div>
+  );
+}
