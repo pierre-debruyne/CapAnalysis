20e2b554a792de46a40c766ac955b5a920bd0480
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-08-29 18:55:27 +0200

    feat(page): add CapactInnovationSoftwarePerformanceInvestments with Hero component

---
+++ src/app/capact-innovation-software-performance-investments/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/common/Hero";
+
+export default function CapactInnovationSoftwarePerformanceInvestments() {
+  return (
+    <div>
+      <Hero
+        imgSrc="/assets/images/img.png"
+        imgAlt=""
+        title="Uncovering the truth behind your wealth management"
+        text="Financial bodyguard & independent forensic analysis"
+      />
+    </div>
+  );
+}
