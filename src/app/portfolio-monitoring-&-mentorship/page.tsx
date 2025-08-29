ed1d167aed3201d56d169748d2effffed99b3936
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-08-29 18:52:25 +0200

    feat(page): add PortfolioMonitoringMentorship with Hero component

---
+++ src/app/portfolio-monitoring-&-mentorship/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/common/Hero";
+
+export default function PortfolioMonitoringMentorship() {
+  return (
+    <div>
+      <Hero
+        imgSrc="/assets/images/hero_pmm.jpeg"
+        imgAlt=""
+        title={`Portfolio Monitoring\n& Mentorship`}
+        text={`Wealth needs guardianship,\nnot just management.`}
+      />
+    </div>
+  );
+}
