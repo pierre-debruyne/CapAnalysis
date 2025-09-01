c305205b7acae738fcdff13589c25bb81b785341
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-01 22:33:21 +0200

    feat: create Checkbox component UI

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import clsx from "clsx";
+
+export interface CheckboxProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, label, ...props }, ref) => {
+    return (
+      <label className="inline-flex items-center gap-2 cursor-pointer select-none">
+        <input
+          type="checkbox"
+          ref={ref}
+          className={clsx(
+            "h-[15px] w-[15px] cursor-pointer appearance-none  bg-white transition-colors focus:ring-2 focus:ring-offset-1 focus:ring-[#806863] disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          {...props}
+        />
+        {label && (
+          <span className="text-sm font-sans font-light text-[#806863]">
+            {label}
+          </span>
+        )}
+      </label>
+    );
+  }
+);
+
+Checkbox.displayName = "Checkbox";
+
+export { Checkbox };
