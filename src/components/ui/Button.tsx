02328a10e0b057981561562cb658c5840fccae3b
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-01 22:33:14 +0200

    fix: modify Button component UI

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,19 +11,16 @@ export default function Button({ className, children }: ButtonProps) {
   return (
     <button
       className={clsx(
-        "flex cursor-pointer items-center text-[11px] font-sans font-light px-2 py-[6px] w-[114px] h-[25px] border border-[#BAACA9] text-[#806863] transition-colors duration-200",
+        "flex cursor-pointer items-center gap-2 text-[11px] font-sans font-light px-3 py-2 min-h-[25px] border border-[#BAACA9] text-[#806863] transition-colors duration-200",
         className
       )}
     >
       <div className="flex items-center text-[#BAACA9]">
         <Plus className="w-[10px] h-[10px]" strokeWidth={1} />
-        <ChevronRight
-          className="w-[14px] h-[14px] -ml-[6px]"
-          strokeWidth={0.6}
-        />
+        <ChevronRight className="w-[14px] h-[14px] -ml-[6px]" strokeWidth={0.6} />
       </div>
 
-      <span>{children}</span>
+      <span className="whitespace-nowrap">{children}</span>
     </button>
-  );
+  )
 }
