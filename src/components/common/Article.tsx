1639de4ef102c3f2b4d97776a5352695550591e3
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-01 19:09:13 +0200

    feat(news): add Article component with image, metadata and button

---
+++ src/components/common/Article.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image"
+import Button from "../ui/Button";
+
+type ArticleProps = {
+  imgSrc: string;
+  imgAlt?: string;
+  title: string;
+  date: string;
+  text: string;
+  text2?: string;
+  posted: string;
+  hashtag?: string;
+};
+
+export default function Article({ imgSrc, imgAlt, title, text, text2, posted, hashtag, date}: ArticleProps) {
+  return (
+    <div className="max-w-[942px] flex border-b border-[#BAACA9] gap-[50px] mx-auto py-[34px]">
+      <div>
+        <div className="w-[207px] h-[207px] border-beige">
+          <Image
+            width={207}
+            height={207}
+            src={imgSrc}
+            alt={imgAlt || title}
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        <div className="flex flex-col gap-[4px] text-[12px] pt-[10px]">
+          <span className="font-light text-[#BAACA9] ">
+            Posted in
+            <span className="font-medium italic text-[#806863]"> {posted}</span>
+          </span>
+          <span className="text-[#806863]">{hashtag}</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="flex flex-col pb-[13px] gap-[13px]">
+          <h2 className="font-serif text-[23px]">{title}</h2>
+          <div className="flex gap-1">
+            <span className="font-sans text-[12px] text-[#BAACA9]">
+              Postedon
+            </span>
+            <span className="font-sans text-[12px] text-[#806863]">{date}</span>
+          </div>
+        </div>
+
+        {/* Paragraphe */}
+        <div className="flex flex-col gap-2 pb-[13px]">
+          <p className="text-[16px] font-sans font-light text-[#806863]">
+            {text}
+          </p>
+          <p className="text-[16px] font-sans font-light text-[#806863]">
+            {text2}
+          </p>
+        </div>
+
+        <Button>READ ARTICLE</Button>
+      </div>
+    </div>
+  );
+}
