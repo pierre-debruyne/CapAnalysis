eede789fd4ff0f22703f6ff11ab4acd049586e6f
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-03 21:59:12 +0200

    feat(nav): add link to logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import NavItem from "./NavItem";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-dark px-[41px] pt-7 pb-[19px]">
       <div className="relative flex items-center">
         {/* Logo */}
-        <Image
-          src="/assets/icons/logo.svg"
-          alt="Logo"
-          width={260}
-          height={56}
-          style={{ width: 260, height: 56 }}
-        />
+
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Logo"
+            width={260}
+            height={56}
+            style={{ width: 260, height: 56 }}
+            priority
+          />
+        </Link>
 
         {/* Navigation */}
         <nav className="absolute z-10 left-1/2 -translate-x-1/2 flex gap-[51px]">
