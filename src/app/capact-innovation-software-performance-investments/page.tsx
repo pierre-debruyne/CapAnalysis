038084b42094477bda55afb3ab75f51d3358f859
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-03 21:40:35 +0200

    feat(page): add CAPact page

--- src/app/capact-innovation-software-performance-investments/page.tsx
+++ src/app/capact-innovation-software-performance-investments/page.tsx
@@ -5,11 +5,181 @@ export default function CapactInnovationSoftwarePerformanceInvestments() {
   return (
     <div>
       <Hero
-        imgSrc="/assets/images/img.png"
+        imgSrc="/assets/images/hero_capact.png"
         imgAlt=""
-        title="Uncovering the truth behind your wealth management"
-        text="Financial bodyguard & independent forensic analysis"
+        title={`CAPAct:\n  our proprietary\n analytical platform`}
       />
+
+      <div className="pt-[45px] pb-[55px]">
+        <div className="max-w-[692px] mx-auto flex gap-[40px]">
+          <div className="font-normal font-serif text-[35px] w-1/2 text-center flex flex-col justify-end">
+            <p className="leading-10 pb-[5px]">
+              At the heart of CAPAnalysis lies CAPAct, our proprietary software
+              developed in-house to deliver high-precision financial analysis.
+            </p>
+            <p className="font-light font-sans text-[#806863] text-[16px] text-start">
+              We have designed an intuitive and powerful suite of tools,
+              supported by robust algorithms, to meticulously track and process
+              every detail of your transactions.
+            </p>
+          </div>
+          <div className="font-light font-sans text-[#806863] text-[16px] w-1/2 flex gap-[13px] flex-col">
+            <p>This methodology applies to two categories of assets:</p>
+            <p>
+              Financial Investments: All publicly traded instruments on global
+              financial markets.
+            </p>
+            <p>
+              Non-Financial Investments: Art collections, real estate, and other
+              tangible assets.
+            </p>
+            <p>
+              CAPAct consolidates and standardizes your entire investment
+              universe, across asset classes, geographies, and sectors, enabling
+              meaningful comparisons, accurate performance tracking, and
+              comprehensive risk assessment.
+            </p>
+            <p>
+              What sets CAPAct apart is its exceptional flexibility and
+              analytical depth, allowing for both granular and aggregated
+              analysis of your data, without limitation.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex w-full">
+        <div className="bg-[#F8F1EC] w-full py-[65px]">
+          <h2 className="text-center text-[23px] font-serif font-normal">
+            Our analysis brings factual financial <br /> context to your
+            managers’ performances
+          </h2>
+          <div className="flex justify-between w-full mx-auto max-w-[1250px] py-[35px] divide-x-[0.6px] divide-[#BAACA9]">
+            <div className="flex-1 max-w-[360px] pr-[45px]">
+              <div className="max-w-[297px]">
+                <div className="text-[15px] uppercase font-sans font-semibold">
+                  Efficiency
+                </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  We ensure the consistent and standardised booking of all your
+                  transactions, across both financial and non-financial assets.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex-1 max-w-[360px] px-[45px]">
+              <div className="max-w-[297px]">
+                <div className="text-[15px] uppercase font-sans font-semibold">
+                  Innovation
+                </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  Our proprietary system continuously monitors your wealth and
+                  delivers clear, concise monthly and ad hoc reports tailored to
+                  your needs.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex-1 max-w-[360px] pl-[45px]">
+              <div className="max-w-[297px]">
+                <div className="text-[15px] uppercase font-sans font-semibold">
+                  Risk Assessment
+                </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  We evaluate your investment performance in real time, placing
+                  it in direct context with market trends and benchmarks.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex-1 max-w-[360px] pl-[45px]">
+              <div className="max-w-[297px]">
+                <div className="text-[15px] font-sans font-semibold">
+                  Fees & Taxes
+                </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  We harmonise and classify all financial costs, income, and
+                  expenses—by beneficiary and cost centre—for full transparency
+                  and control.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="pt-[51px] text-center">
+        <h2 className="text-[23px] font-serif font-normal pb-[19px]">
+          Our safety measures
+        </h2>
+        <div className="font-sans font-light text-[16px] text-[#806863] pb-[8px]">
+          Your data is safe with us.
+        </div>
+        <p className="font-sans font-light text-[16px] text-[#806863]">
+          CAPAnalysis adheres strictly to Swiss banking secrecy laws and treats
+          <br />
+          all client information with the highest level of confidentiality. To
+          ensure <br />
+          your data remains secure, we apply the following key principles:
+        </p>
+
+        <div className="flex text-start justify-between w-full mx-auto max-w-[1250px] py-[70px] divide-x-[0.6px] divide-[#BAACA9]">
+          <div className="flex-1 max-w-[294px] pr-[45px]">
+            <div className="max-w-[294px]">
+              <div className="text-[15px] uppercase font-sans font-semibold">
+                Absolute Discretion
+              </div>
+              <p className="font-sans font-light text-[16px] text-[#806863]">
+                Discretion is at the core of our philosophy. Within our CAPAct
+                system, clients are identified exclusively by code names.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex-1 max-w-[294px]">
+            <div className="max-w-[294px] px-[34px]">
+              <div className="text-[15px] uppercase font-sans font-semibold">
+                Data Consolidation in Switzerland
+              </div>
+              <p className="font-sans font-light text-[16px] text-[#806863] ">
+                All data consolidation activities are carried out exclusively on
+                our premises in Geneva, Switzerland.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex-1 max-w-[294px]">
+            <div className="max-w-[294px] px-[34px]">
+              <div className="text-[15px] uppercase font-sans font-semibold">
+                Secure Data Handling Process
+              </div>
+              <p className="font-sans font-light text-[16px] text-[#806863]">
+                Our process includes three key steps:
+              </p>
+              <ul className="list-disc list-outside pl-5 font-sans font-light text-[16px] text-[#806863]">
+                <li>Data aggregation</li>
+                <li>Electronic anonymisation of transaction records.</li>
+                <li>
+                  Physical destruction of original tickets to eliminate any
+                  residual risk.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex-1 max-w-[294px]">
+            <div className="max-w-[294px] px-[34px]">
+              <div className="text-[15px] uppercase font-sans font-semibold">
+                Highly Secured Infrastructure
+              </div>
+              <p className="font-sans font-light text-[16px] text-[#806863]">
+                All servers are hosted in a state-of-the-art, highly secured
+                facility located entirely on Swiss territory
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <Footer />
     </div>
