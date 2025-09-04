246395fd48e0db73d07da792005175c8eacc8245
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-04 17:10:23 +0200

    style: add style for html and body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="h-full w-full" lang="en">
       <body className="font-sans">
         <Header />
         {children}
