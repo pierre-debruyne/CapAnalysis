a24c87c99292e4ae66cdfb068bdec6e46bc85d52
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-04 21:04:37 +0200

    feat(footer): make it responsive

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,21 +5,21 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="w-full h-[73px] bg-dark flex justify-center gap-[35px] items-center mx-auto">
+    <div className="w-full lg:h-[73px] bg-dark flex lg:flex-row flex-col justify-center p-5 gap-5 lg:gap-[35px] items-center mx-auto">
       <div className="flex gap-2">
-        <MapPin className="text-brown" />
-        <span className="text-[16px] font-sans font-light text-white">
+        <MapPin className="text-brown hidden lg:block" />
+        <span className="text-[16px] font-sans font-light text-white text-center">
           118 rue du Rhône - 1204 Geneva - Switzerland
         </span>
       </div>
       <div className="flex gap-2">
-        <Smartphone className="text-brown" />
+        <Smartphone className="text-brown hidden lg:block" />
         <Link href="tel:+ 41 (0) 22 321 90 90" className="text-[16px] font-sans font-light text-white hover:underline">
           + 41 (0) 22 321 90 90
         </Link>
       </div>
       <div className="flex gap-2">
-        <AtSign className="text-brown" />
+        <AtSign className="text-brown hidden lg:block" />
         <a
           href="mailto:contact@exemple.com"
           className="hover:underline text-[16px] font-sans font-light text-white"
