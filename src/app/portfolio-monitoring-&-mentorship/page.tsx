fb3cc178d63cf747a667318c7a1675bf5420e484
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-05 23:58:10 +0200

    fix: responsive portfolio page

--- src/app/portfolio-monitoring-&-mentorship/page.tsx
+++ src/app/portfolio-monitoring-&-mentorship/page.tsx
@@ -13,7 +13,7 @@ export default function PortfolioMonitoringMentorship() {
         imgPosition="object-[70%_50%] md:object-[20%_50%]"
       />
 
-      <div className="pt-[67px] pb-[48px]">
+      <div className="pt-[67px] pb-[48px] px-[50px]">
         <h2 className="text-[35px] font-serif text-center text-[#292929] leading-11 pb-[10px]">
           Independent financial expertise for
           <br /> control, protection, and clarity.
@@ -102,33 +102,34 @@ export default function PortfolioMonitoringMentorship() {
           <h2 className="text-center text-[23px] font-serif font-normal">
             Verification - Assessment - Comparison
           </h2>
-          <div className="flex flex-col gap-5 xl:gap-0 xl:flex-row items-center xl:justify-between w-full mx-auto max-w-[1166px] py-[35px] xl:divide-x-[0.6px] xl:divide-[#BAACA9]">
-              <div className="flex-1 max-w-[360px] xl:pr-[45px] text-center lg:text-start">
-                <div className="xl:max-w-[297px]">
-                  <div className="text-[15px] uppercase font-sans font-semibold">
-                    Risks
-                  </div>
-                  <p className="font-sans font-light text-[16px] text-[#806863]">
-                    Our risk staging allow you to be pro-actively prepared to
-                    financial markets erratic behaviours and think through
-                    efficiently your presence on the market.
-                  </p>
+          <div className="w-full mx-auto max-w-[1166px] py-[35px] flex flex-col gap-5 md:grid md:grid-cols-2 md:justify-items-center xl:flex xl:flex-row xl:items-stretch xl:gap-0 xl:divide-x-[0.6px] xl:divide-[#BAACA9]">
+            <div className="w-full md:w-auto xl:flex-1 xl:h-full xl:flex xl:pr-[45px] text-center xl:text-start md:justify-self-center">
+              <div className="max-w-[360px] xl:max-w-[297px] mx-auto xl:mx-0">
+                <div className="text-[15px] uppercase font-sans font-semibold">
+                  Risks
                 </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  Our risk staging allow you to be pro-actively prepared to
+                  financial markets erratic behaviours and think through
+                  efficiently your presence on the market.
+                </p>
               </div>
-              <div className="flex-1 max-w-[360px] xl:px-[45px] text-center lg:text-start">
-                <div className="xl:max-w-[297px]">
-                  <div className="text-[15px] uppercase font-sans font-semibold">
-                    Performance
-                  </div>
-                  <p className="font-sans font-light text-[16px] text-[#806863]">
-                    Our utmost mission is to demonstrate with sound elements
-                    what contributed to your performance.
-                  </p>
-                </div>
-              </div>
+            </div>
 
-            <div className="flex-1 max-w-[360px] xl:pl-[45px] text-center lg:text-start">
-              <div className="xl:max-w-[297px]">
+            <div className="w-full md:w-auto xl:flex-1 xl:h-full xl:flex xl:px-[45px] text-center xl:text-start md:justify-self-center">
+              <div className="max-w-[360px] xl:max-w-[297px] mx-auto xl:mx-0 xl:h-[119px]">
+                <div className="text-[15px] uppercase font-sans font-semibold">
+                  Performance
+                </div>
+                <p className="font-sans font-light text-[16px] text-[#806863]">
+                  Our utmost mission is to demonstrate with sound elements what
+                  contributed to your performance.
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full md:w-auto md:col-span-2 md:justify-self-center xl:flex-1 xl:h-full xl:flex xl:pl-[45px] text-center xl:text-start">
+              <div className="max-w-[360px] xl:max-w-[297px] mx-auto xl:mx-0">
                 <div className="text-[15px] uppercase font-sans font-semibold">
                   Fees & Taxes
                 </div>
@@ -140,6 +141,7 @@ export default function PortfolioMonitoringMentorship() {
               </div>
             </div>
           </div>
+
           <p className="font-sans text-[16px] font-normal text-center">
             We enable superior control and more transparency
             <br />
@@ -159,27 +161,24 @@ export default function PortfolioMonitoringMentorship() {
           and volume of the following elements:
         </p>
 
-        <div className="flex flex-col 2xl:flex-row text-start  2xl:items-stretch items-center gap-5 2xl:gap-0 justify-between w-full mx-auto max-w-[1472px] py-[70px] 2xl:divide-x-[0.6px] 2xl:divide-[#BAACA9]">
-          <div className="flex-1 w-2/3 2xl:max-w-[294px] 2xl:pr-[45px]">
-            <div className="2xl:max-w-[294px]">
+        <div className="w-full mx-auto max-w-[1472px] py-[70px] flex flex-col gap-10 px-[50px]">
+          <div className="flex flex-col gap-5  md:grid md:grid-cols-2 md:gap-10 md:justify-items-center md:max-w-[900px] md:mx-auto 2xl:flex 2xl:flex-row 2xl:items-stretch 2xl:gap-0 2xl:max-w-none 2xl:divide-x-[0.6px] 2xl:divide-[#BAACA9]">
+            <div className="max-w-[360px] 2xl:max-w-[294px] w-full mx-auto text-center md:text-start 2xl:flex-1 2xl:px-[34px]">
               <div className="text-[15px] uppercase font-sans font-semibold">
-                CUSTODIANS
+                Custodians
               </div>
               <p className="font-sans font-light text-[16px] text-[#806863]">
-                Where are your assets held?
-                <br />
+                Where are your assets held? <br />
                 We work with a wide range of counterparties, including banks,
                 prime brokers, and insurance companies.
               </p>
             </div>
-          </div>
 
-          <div className="flex-1 w-2/3 2xl:max-w-[294px]">
-            <div className="2xl:max-w-[294px] 2xl:px-[34px]">
+            <div className="max-w-[360px] 2xl:max-w-[294px] w-full mx-auto text-center md:text-start 2xl:flex-1 2xl:px-[34px]">
               <div className="text-[15px] uppercase font-sans font-semibold">
-                PORTFOLIOS
+                Portfolios
               </div>
-              <p className="font-sans font-light text-[16px] text-[#806863] ">
+              <p className="font-sans font-light text-[16px] text-[#806863]">
                 Our system consolidates both financial (e.g. stocks, bonds,
                 funds), and non-financial assets (e.g. real estate,
                 collections).
@@ -189,24 +188,20 @@ export default function PortfolioMonitoringMentorship() {
                 independent sources.
               </p>
             </div>
-          </div>
 
-          <div className="flex-1 w-2/3 2xl:max-w-[294px]">
-            <div className="2xl:max-w-[294px] 2xl:px-[34px]">
+            <div className="max-w-[360px] 2xl:max-w-[294px] w-full mx-auto text-center md:text-start 2xl:flex-1 2xl:px-[34px]">
               <div className="text-[15px] uppercase font-sans font-semibold">
-                INSTRUMENTS
+                Instruments
               </div>
               <p className="font-sans font-light text-[16px] text-[#806863]">
                 We monitor and report the fees and taxes you were charged
                 following your custodian agreements and fiscal status/domicile.
               </p>
             </div>
-          </div>
 
-          <div className="flex-1 w-2/3 2xl:max-w-[294px]">
-            <div className="2xl:max-w-[294px] 2xl:px-[34px]">
+            <div className="max-w-[360px] 2xl:max-w-[294px] w-full mx-auto text-center md:text-start 2xl:flex-1 2xl:px-[34px]">
               <div className="text-[15px] uppercase font-sans font-semibold">
-                TRANSACTIONS
+                Transactions
               </div>
               <p className="font-sans font-light text-[16px] text-[#806863]">
                 We record all transactions across your portfolios and
@@ -214,26 +209,23 @@ export default function PortfolioMonitoringMentorship() {
                 accuracy.
               </p>
             </div>
-          </div>
 
-          <div className="flex-1 w-2/3 2xl:max-w-[294px]">
-            <div className=" 2xl:max-w-[294px] 2xl:px-[34px]">
+            <div className="max-w-[360px] 2xl:max-w-[294px] w-full mx-auto text-center md:text-start md:col-span-2 md:justify-self-center 2xl:flex-1 2xl:px-[34px]">
               <div className="text-[15px] uppercase font-sans font-semibold">
-                TYPE OF REPORTS
+                Type of Reports
               </div>
               <p className="font-sans font-light text-[16px] text-[#806863]">
                 Upon request, we produce a variety of reports:
               </p>
-              <ul className="font-sans font-light text-[16px] text-[#806863]">
+              <ul className="font-sans font-light text-[16px] text-[#806863] space-y-2">
                 <li>
-                  <span className="font-semibold text-[16px]">Analytical</span>{" "}
+                  <span className="font-semibold">Analytical</span>
                   snapshots highlighting key performance drivers and notable
                   financial events
                 </li>
                 <li>
-                  <span className="font-semibold text-[16px]">
+                  <span className="font-semibold">
                     In-depth analyses detailing
-                    <br />
                   </span>
                   performance attribution and risk exposure across your
                   investments
