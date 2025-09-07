fa1b2302d320793455bbcabba271df3d3c913b1c
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-07 15:34:42 +0200

    fix(hero): responsive for mobile

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -10,20 +10,45 @@ type HeroProps = {
 
 export default function Hero({ imgSrc, imgAlt, title, text, imgPosition }: HeroProps) {
   return (
-    <div className="w-full h-[692px] relative overflow-x-clip mt-[87px]">
+<div className="relative w-full h-[692px] overflow-x-clip mt-[87px] md:mt-0 mb-[335px] md:mb-0">
+      <div className="relative w-full block md:hidden bg-dark">
+        <Image
+          src="/assets/images/logo.png"
+          alt=""
+          width={376}
+          height={450}
+          unoptimized
+          className="w-[280px] h-auto mx-auto"
+        />
+
+        <div
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+               text-center w-full px-6"
+        >
+          <h2 className="whitespace-pre-line text-beige text-[35px] pb-[15px] font-serif font-normal leading-none">
+            {title}
+          </h2>
+          <p className="whitespace-pre-line text-beige md:text-[21px] mx-8 font-sans font-light leading-none">
+            {text}
+          </p>
+        </div>
+      </div>
+
       <div className="w-full lg:flex h-full">
         <div className="xl:w-[69%] w-full relative h-full">
           <Image
             src={imgSrc}
             alt={imgAlt || title}
             fill
-            className={["object-cover bg-dark" , imgPosition].filter(Boolean).join(" ")}
+            className={["object-cover bg-dark", imgPosition]
+              .filter(Boolean)
+              .join(" ")}
           />
         </div>
         <div className="bg-dark w-[31%] xl:block hidden" />
       </div>
 
-      <div className="absolute right-0 top-[142px] bottom-[95px] xl:w-[54%] bg-dark rounded-s-[230px] flex flex-col justify-center overflow-hidden pl-[45px] pt-[35px] box-border">
+      <div className="md:absolute hidden right-0 top-[142px] bottom-[95px] md:w-[54%] bg-dark rounded-s-[230px] md:flex flex-col justify-center overflow-hidden pl-[45px] pt-[35px] box-border">
         <Image
           className="mt-[31px]"
           src="/assets/images/logo.png"
