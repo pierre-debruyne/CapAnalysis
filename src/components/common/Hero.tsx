afc12ff91004665da0e0ec5a815654ce79efa244
Mehdi Chaibi <mehdichaibi.dev@gmail.com>
2025-09-08 23:23:03 +0200

    fix(hero): make it responsive for full screen

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -10,7 +10,8 @@ type HeroProps = {
 
 export default function Hero({ imgSrc, imgAlt, title, text, imgPosition }: HeroProps) {
   return (
-<div className="relative w-full aspect-video min-h-[350px] md:h-[692px] overflow-x-clip mt-[87px] md:mt-0 mb-[335px] md:mb-0 xl:mt-[95px]">      <div className="relative w-full block md:hidden bg-dark">
+    <div className="relative w-full aspect-video min-h-[350px] md:h-[692px] overflow-x-clip mt-[87px] md:mt-0 mb-[335px] md:mb-0 xl:mt-[95px]">
+      <div className="relative w-full block md:hidden bg-dark">
         <Image
           src="/assets/images/logo.png"
           alt=""
@@ -32,7 +33,6 @@ export default function Hero({ imgSrc, imgAlt, title, text, imgPosition }: HeroP
           </p>
         </div>
       </div>
-
       <div className="w-full lg:flex h-full">
         <div className="xl:w-[69%] w-full relative h-full">
           <Image
@@ -46,7 +46,6 @@ export default function Hero({ imgSrc, imgAlt, title, text, imgPosition }: HeroP
         </div>
         <div className="bg-dark w-[31%] xl:block hidden" />
       </div>
-
       <div className="md:absolute hidden right-0 top-[142px] bottom-[95px] md:w-[54%] bg-dark rounded-s-[230px] md:flex flex-col justify-center overflow-hidden pl-[45px] pt-[35px] box-border">
         <Image
           className="mt-[31px]"
@@ -57,7 +56,7 @@ export default function Hero({ imgSrc, imgAlt, title, text, imgPosition }: HeroP
           unoptimized
         />
 
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-full">
+        <div className=" absolute inset-0 flex flex-col items-center justify-center text-center 2xl:ml-[50px] max-w-[900px] mx-auto">
           <h2 className="whitespace-pre-line w-full text-beige text-[35px] xl:text-[60px] pb-[45px] font-serif font-normal leading-none">
             {title}
           </h2>
