032909dee07e24f0b6777099bbb7eb475b4762ee
Pierre Debruyne <pierre.debruyne@me.com>
2025-09-09 22:35:44 +0200

    update erreur import

--- src/components/common/InfoBlock.tsx
+++ src/components/common/InfoBlock.tsx
@@ -1,26 +1,20 @@
-import { ChevronRight } from 'lucide-react';
-import React from 'react'
+import React from "react";
 
 type InfoBlockProps = {
-    UppercaseText: React.ReactNode; 
-    LowercaseText: React.ReactNode;
-  };
-  
-  export default function InfoBlock({ UppercaseText, LowercaseText}: InfoBlockProps) { 
+  UppercaseText: React.ReactNode;
+  LowercaseText: React.ReactNode;
+};
 
+export default function InfoBlock({ UppercaseText, LowercaseText }: InfoBlockProps) {
   return (
-    <div className="flex">
-      <div className="flex flex-col items-center">
-        <div className="w-px bg-[#BAACA9] flex-1 -mt-[5px] -ml-[5px]" />
+    <div className='flex'>
+      <div className='flex flex-col items-center'>
+        <div className='w-px bg-[#BAACA9] flex-1 -mt-[5px] -ml-[5px]' />
       </div>
 
-      <div className="md:max-w-[276px] ml-3">
-        <span className="uppercase text-[#2D2D2D] whitespace-pre-line font-sans font-semibold text-[18px]">
-          {UppercaseText}
-        </span>
-        <span className="font-light font-sans text-[16px] text-[#806863]">
-          {LowercaseText}
-        </span>
+      <div className='md:max-w-[276px] ml-3'>
+        <span className='uppercase text-[#2D2D2D] whitespace-pre-line font-sans font-semibold text-[18px]'>{UppercaseText}</span>
+        <span className='font-light font-sans text-[16px] text-[#806863]'>{LowercaseText}</span>
       </div>
     </div>
   );
